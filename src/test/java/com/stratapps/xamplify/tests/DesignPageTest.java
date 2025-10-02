f38f045a83eb5939b2a2c4fe082c382ac24f2327
Ganeshkalagara7 <kalagaraganesh@stratapps.com>
2025-10-02 16:33:32 +0530

    Design Pages final changes

--- src/test/java/com/stratapps/xamplify/tests/DesignPageTest.java
+++ src/test/java/com/stratapps/xamplify/tests/DesignPageTest.java
@@ -32,121 +32,142 @@ public class DesignPageTest extends BaseTest{
 //    	designPages = new DesignPages(driver);   
 //    }
 	
-	  @Test(priority = 1, enabled = true) 
-	  public void CreateRegularPublicPage()throws InterruptedException {
-	  System.out.println(" ");
-	  logger.info("Create Regular Public Page in Design Pages");
-	  designPages.designDesignPage(); 
-	  Thread.sleep(4000);
-	  designPages.RegularTab();
-	  Thread.sleep(4000);
-	  designPages.createPage(); 
-	  designPages.regularpublicPageSaveandClose();
-	  logger.info("Successfully Created Regular Public Page in Design Pages");
-	  System.out.println(" "); }
-	  
-	  @Test(priority = 2, enabled = true) 
-	  public void CreateRegularPrivatePage()throws InterruptedException {
-	  logger.info("Create Regular Private Page in Design Pages");
-	  designPages.designDesignPage(); 
-	  designPages.RegularTab();
-	  designPages.createPage(); 
-	  designPages.regularPrivatePageSaveandClose();
-	  logger.info("Successfully Created Regular Private Page in Design Pages");
-	  System.out.println(" "); }
-	  
-	  @Test(priority = 3, enabled = true) 
-	  public void CreateCobrandedPublicPage()
-	  throws InterruptedException {
-	  logger.info("Create Cobranded Public Page in Design Pages");
-	  designPages.designDesignPage(); 
-	  designPages.cobrandedTab();
-	  designPages.createPage(); 
-	  designPages.cobrandedpublicpagecreation();
-	  logger.info("Successfully Created Cobranded Public Page in Design Pages");
-	  System.out.println(" "); }
-	  
-	  @Test(priority = 4, enabled = true) 
-	  public void CreateCobrandedPrivatePage()
-	  throws InterruptedException {
-	  logger.info("Create Cobranded Private Page in Design Pages");
-	  designPages.designDesignPage(); 
-	  designPages.cobrandedTab();
-	  designPages.createPage(); 
-	  designPages.cobrandedprivatepagecreation();
-	  logger.info("Successfully Created Cobranded Private Page in Design Pages");
-	  System.out.println(" "); }
-	 
-	 
+		
+		  @Test(priority = 1, enabled = true) public void
+		  CreateRegularPublicPage()throws InterruptedException {
+		  System.out.println(" ");
+		  logger.info("Create Regular Public Page in Design Pages");
+		  designPages.designDesignPage(); Thread.sleep(4000); designPages.RegularTab();
+		  Thread.sleep(4000); designPages.createPage();
+		  designPages.regularpublicPageSaveandClose();
+		  logger.info("Successfully Created Regular Public Page in Design Pages");
+		  System.out.println(" "); }
+		  
+		  @Test(priority = 2, enabled = true) public void
+		  CreateRegularPrivatePage()throws InterruptedException {
+		  logger.info("Create Regular Private Page in Design Pages");
+		  designPages.designDesignPage(); designPages.RegularTab();
+		  designPages.createPage(); designPages.regularPrivatePageSaveandClose();
+		  logger.info("Successfully Created Regular Private Page in Design Pages");
+		  System.out.println(" "); }
+		  
+		  @Test(priority = 3, enabled = true) public void
+		  CreateRegularProtectedPage()throws InterruptedException {
+		  logger.info("Create Regular Protect Page in Design Pages");
+		  designPages.designDesignPage(); designPages.RegularTab();
+		  designPages.createPage(); designPages.regularProtectedPageSaveandClose();
+		  logger.info("Successfully Created Regular Protect Page in Design Pages");
+		  System.out.println(" "); }
+		  
+		  @Test(priority = 4, enabled = true) public void CreateCobrandedPublicPage()
+		  throws InterruptedException {
+		  logger.info("Create Cobranded Public Page in Design Pages");
+		  designPages.designDesignPage(); designPages.cobrandedTab();
+		  designPages.createPage(); designPages.cobrandedpublicpagecreation();
+		  logger.info("Successfully Created Cobranded Public Page in Design Pages");
+		  System.out.println(" "); }
+		  
+		  @Test(priority = 5, enabled = true) public void CreateCobrandedPrivatePage()
+		  throws InterruptedException {
+		  logger.info("Create Cobranded Private Page in Design Pages");
+		  designPages.designDesignPage(); designPages.cobrandedTab();
+		  designPages.createPage(); designPages.cobrandedprivatepagecreation();
+		  logger.info("Successfully Created Cobranded Private Page in Design Pages");
+		  System.out.println(" "); }
+		  
+		  @Test(priority = 6, enabled = true) public void
+		  CreateCobrandedProtectedPage() throws InterruptedException {
+		  logger.info("Create Cobranded Protect Page in Design Pages");
+		  designPages.designDesignPage(); designPages.cobrandedTab();
+		  designPages.createPage(); designPages.cobrandedProtectedPageSaveandClose();
+		  logger.info("Successfully Created Cobranded Protect Page in Design Pages");
+		  System.out.println(" "); }
+		 	
+		  @Test(priority = 7, enabled = true) 
+		  public void regularpublicpage() throws
+		  InterruptedException, UnsupportedFlavorException, IOException {
+		  logger.info("Regular Public Page Actions in Manage Pages");
+		  designPages.designManagePage(); 
+		  designPages.PageTab("RegularPublic");
+		  designPages.searchPage(); 
+		  designPages.copyPage(); 
+		  designPages.editpage();
+		  designPages.CopyAndEmbeddedPage(); 
+		  designPages.PreviewPage();
+		  designPages.PageAnalytics(); 
+		  designPages.DeletePage(); logger.
+		  info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Regular Public Page in Manage Pages"
+		  ); System.out.println(" "); }
+		  
+		  @Test(priority = 8, enabled = true) public void regularprivatepage() throws
+		  InterruptedException, UnsupportedFlavorException, IOException {
+		  System.out.println(" ");
+		  logger.info("Create Regular Private Page in Design Pages");
+		  designPages.designManagePage(); designPages.PageTab("RegularPrivate");
+		  designPages.searchPage(); designPages.copyPage(); 
+		   designPages.editpage();
+		  designPages.CopyAndEmbeddedPage(); designPages.PreviewPage();
+		  designPages.PageAnalytics(); designPages.DeletePage(); logger.
+		  info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Regular Private Page in Manage Pages"
+		  ); System.out.println(" "); }
+		  
+		  @Test(priority = 9, enabled = true) 
+		  public void regularprotectpage() throws
+		  InterruptedException, UnsupportedFlavorException, IOException {
+		  System.out.println(" ");
+		  logger.info("Create Regular Protect Page in Design Pages");
+		  designPages.designManagePage();
+		  designPages.PageTab("RegularProtect");
+		  designPages.searchPage();
+		  designPages.copyPage();
+		   designPages.editpage();
+		  designPages.ProtectPageCopyURL();
+		  designPages.PreviewPage();
+		  designPages.ProtectPageAnalytics();
+		  designPages.DeletePage(); logger.
+		  info("Successfully copy, edit, preview, delete, Copy Pages & Analytics actions for Regular protect Page in Manage Pages"
+		  ); System.out.println(" "); }
+		  
+		  @Test(priority = 10, enabled = true) public void cobrandedpublicpage() throws
+		  InterruptedException, UnsupportedFlavorException, IOException {
+		  System.out.println(" ");
+		  logger.info("Create Cobranded public Page in Design Pages");
+		  designPages.designManagePage(); designPages.PageTab("CoBrandedPublic");
+		  designPages.searchPage(); designPages.copyPage(); 
+		   designPages.editpage();
+		  designPages.CopyAndEmbeddedPage(); designPages.PreviewPage();
+		  designPages.PageAnalytics(); designPages.DeletePage(); logger.
+		  info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Cobranded Public Page in Manage Pages"
+		  ); System.out.println(" "); }
+		  
+		  @Test(priority = 11, enabled = true) public void cobrandedprivatepage()
+		  throws InterruptedException, UnsupportedFlavorException, IOException {
+		  System.out.println(" ");
+		  logger.info("Create Cobranded Private Page in Design Pages");
+		  designPages.designManagePage(); designPages.PageTab("CoBrandedPrivate");
+		  designPages.searchPage(); designPages.copyPage(); 
+		   designPages.editpage();
+		  designPages.CopyAndEmbeddedPage(); designPages.PreviewPage();
+		  designPages.PageAnalytics(); designPages.DeletePage(); logger.
+		  info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for CoBranded Private Page in Manage Pages"
+		  ); System.out.println(" "); }
+		 
 
-	  @Test(priority = 5, enabled = false) 
-	  public void regularpublicpage() throws  InterruptedException, UnsupportedFlavorException, IOException {
-	  logger.info("Regular Public Page Actions in Manage Pages");
-	  designPages.designManagePage(); 
-	  designPages.PageTab("RegularPublic");
-	  designPages.searchPage(); 
-	  designPages.copyPage(); 
-	 // designPages.editpage();
-	  designPages.CopyAndEmbeddedPage();
-	  designPages.PreviewPage(); 
-	  designPages.PageAnalytics(); 
-	  designPages.copyPage(); 
-	  designPages.DeletePage(); 
-	  logger.info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Regular Public Page in Manage Pages"); System.out.println(" "); 
-	  }
-	  
-	  @Test(priority = 6, enabled = false) 
-	  public void regularprivatepage() throws
+	  @Test(priority = 12, enabled = true) 
+	  public void cobrandedprotectpage() throws
 	  InterruptedException, UnsupportedFlavorException, IOException {
 	  System.out.println(" ");
-	  logger.info("Create Regular Private Page in Design Pages");
+	  logger.info("Create Cobranded protect Page in Design Pages");
 	  designPages.designManagePage(); 
-	  designPages.PageTab("RegularPrivate");
+	  designPages.PageTab("CoBrandedProtect");
 	  designPages.searchPage(); 
 	  designPages.copyPage(); 
 	  designPages.editpage();
-	  designPages.CopyAndEmbeddedPage();
+	  designPages.ProtectPageCopyURL();
 	  designPages.PreviewPage(); 
-	  designPages.PageAnalytics(); 
-	  designPages.DeletePage(); 
-	  logger.info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Regular Private Page in Manage Pages"); 
-	  System.out.println(" "); 
-	  }
-	  
-	  @Test(priority = 7, enabled = false) 
-	  public void cobrandedpublicpage() throws
-	  InterruptedException, UnsupportedFlavorException, IOException {
-	  System.out.println(" ");
-	  logger.info("Create Cobranded public Page in Design Pages");
-	  designPages.designManagePage(); 
-	  designPages.PageTab("CoBrandedPublic");
-	  designPages.searchPage(); 
-	  designPages.copyPage(); 
-	  designPages.editpage();
-	  designPages.CopyAndEmbeddedPage();
-	  designPages.PreviewPage(); 
-	  designPages.PageAnalytics(); 
-	  designPages.DeletePage(); 
-	  logger.info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for Cobranded Public Page in Manage Pages"); 
-	  System.out.println(" "); 
-	  }
-	  
-	  @Test(priority = 8, enabled = false) 
-	  public void cobrandedprivatepage() throws
-	  InterruptedException, UnsupportedFlavorException, IOException {
-	  System.out.println(" ");
-	  logger.info("Create Cobranded Private Page in Design Pages");
-	  designPages.designManagePage(); 
-	  designPages.PageTab("CoBrandedPrivate");
-	  designPages.searchPage(); 
-	  designPages.copyPage(); 
-	  designPages.editpage();
-	  designPages.CopyAndEmbeddedPage();
-	  designPages.PreviewPage(); 
-	  designPages.PageAnalytics(); 
+	  designPages.ProtectPageAnalytics(); 
 	  designPages.DeletePage(); logger.
-	  info("Successfully copy, edit, preview, delete, Copy And Embedded Pages & Analytics actions for CoBranded Private Page in Manage Pages"); 
+	  info("Successfully copy, edit, preview, delete, Copy Pages & Analytics actions for CoBranded Private Page in Manage Pages"); 
 	  System.out.println(" "); 
 	  }
-
 }
